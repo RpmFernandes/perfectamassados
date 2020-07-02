9336967de8ea5418d9de2cf81d4dc8a7f6f47289
Ronald Fernandes <rfernandes@globalcommerce.com.br>
2020-07-02 18:27:26 -0300

    home page image change

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "mclaren.png" }) {
+      placeholderImage: file(relativePath: { eq: "porsche.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
